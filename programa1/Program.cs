63bc7469ba3cc360ad3bdd8d7f899126a9f0df76
chuyano <spawner.astral@hotmail.com>
2014-09-23 19:46:15 -0500

    pedir datos de entrada

--- programa1/Program.cs
+++ programa1/Program.cs
@@ -14,8 +14,21 @@ namespace programa1
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
+			int Entero1, Entero2, Entero3; //Declaramos enteros
+            string Numero1, Numero2; //Declaramos Strings;
+            // Ingresamos el valor 1
+            Console.Write("Ingrese un Numero: "); 
+            Numero1 = Console.ReadLine();
+            // Ingresamos el valor 2
+            Console.Write("Ingrese otro: "); 
+            Numero2 = Console.ReadLine();
+            //Convertimos String a Decimal
+            Entero1 = Int32.Parse(Numero1); 
+            Entero2 = Int32.Parse(Numero2);
+            //Sumamos
+            Entero3 = Entero1 + Entero2;
+            Console.Write("El resultado de la suma es: {0}", Entero3);
+            
 			// TODO: Implement Functionality Here
 			
 			Console.Write("Press any key to continue . . . ");
